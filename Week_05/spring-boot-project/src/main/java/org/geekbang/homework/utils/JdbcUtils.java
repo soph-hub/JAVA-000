861969404168e92ba7949be6f8b3ccc89c9205f9
tyy <edison.yy@outlook.com>
2020-11-19 22:59:23 +0800

    1. Week05（周四）作业3: 代码实现 Spring Bean 的装配 2. Week05（周六）作业4: 实现自动配置和 Starter, 提交的是之前给公司写的 motan-spring-boot-starter 3. JDBC 接口和数据库连接池，掌握它们的设计和用法

--- Week_05/spring-boot-project/src/main/java/org/geekbang/homework/utils/JdbcUtils.java
+++ Week_05/spring-boot-project/src/main/java/org/geekbang/homework/utils/JdbcUtils.java
@@ -1,8 +1,9 @@
 package org.geekbang.homework.utils;
 
+import com.zaxxer.hikari.HikariDataSource;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+import javax.sql.DataSource;
 
 public class JdbcUtils {
 
@@ -11,36 +12,52 @@ public class JdbcUtils {
      */
     private static ThreadLocal<Connection> tl = new ThreadLocal<>();
 
+    private static DataSource dataSource = new HikariDataSource();
 
+    public static DataSource getDataSource() {
+        return dataSource;
+    }
     /**
      * dao使用本方法来获取连接
      *
      * @return connection connection
      */
+//    public static Connection getConnection() throws SQLException {
+//        Connection connection = tl.get();//获取当前线程的事务连接
+//        if (connection != null) {
+//            return connection;
+//        }
+//        try {
+//            Class.forName("com.mysql.cj.jdbc.Driver");
+//        } catch (ClassNotFoundException e) {
+//            System.out.println("Can't find mysql jdbc driver");
+//            e.printStackTrace();
+//        }
+//        try {
+//            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc", "root", "root");
+//            if (connection != null) {
+//                tl.set(connection);
+//                System.out.println("Connection successful!");
+//            } else {
+//                System.out.println("Connection failed!");
+//            }
+//        } catch (SQLException e) {
+//            System.out.println("Connection failed!");
+//            e.printStackTrace();
+//        }
+//        return connection;
+//    }
+
     public static Connection getConnection() throws SQLException {
-        Connection connection = tl.get();//获取当前线程的事务连接
-        if (connection != null) {
-            return connection;
+        /*
+         * 如果有事务，返回当前事务的con
+         * 如果没有事务，通过连接池返回新的con
+         */
+        Connection con = tl.get();//获取当前线程的事务连接
+        if (con != null) {
+            return con;
         }
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            System.out.println("Can't find mysql jdbc driver");
-            e.printStackTrace();
-        }
-        try {
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc", "root", "root");
-            if (connection != null) {
-                tl.set(connection);
-                System.out.println("Connection successful!");
-            } else {
-                System.out.println("Connection failed!");
-            }
-        } catch (SQLException e) {
-            System.out.println("Connection failed!");
-            e.printStackTrace();
-        }
-        return connection;
+        return getDataSource().getConnection();
     }
 
     /**
